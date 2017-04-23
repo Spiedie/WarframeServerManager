406d8ab4cae71fea546ef79b57ae9559c61a7c4f
Spiedie <WarframeCloudHost@gmail.com>
2017-04-23 12:25:37 -0700

    changed encryption key naming to avoid conflicts

--- Release/WFAllocManagerLocal/WFAllocManager.cmd
+++ Release/WFAllocManagerLocal/WFAllocManager.cmd
@@ -1,4 +1,4 @@
-title WFAllocManagerLocal
+title WFAllocManagerLocal0.1.9.4A
 cd /D %~dp0
-java -jar WFAllocManager0.1.9.3A.jar
+java -jar WFAllocManager0.1.9.4A.jar
 pause
--- Release/WFServerLoginLocal/WFServerLogin.cmd
+++ Release/WFServerLoginLocal/WFServerLogin.cmd
@@ -1,4 +1,4 @@
-title WFServerLoginLocal
+title WFServerLoginLocal0.2.7.6A
 cd /D %~dp0
-java -Xmx128M -jar WFServerLogin0.2.7.5A.jar
+java -Xmx128M -jar WFServerLogin0.2.7.6A.jar
 pause
